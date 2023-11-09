6dcc5776e0251d45f5896bc577699a2c1d839bab
Michael Ward <mdwmage@proton.me>
2023-11-09 13:20:07 -0500

    two-half equations complete

--- src/physicsSolver.nim
+++ src/physicsSolver.nim
@@ -1,6 +1,6 @@
 # Evaluates to a SUVAT Value based on input
 
-import strutils#, std/terminal
+import strutils, std/math#, std/terminal
 
 # basic yes/no question answer machine
 proc y_n(question: string): bool =
@@ -15,9 +15,8 @@ proc y_n(question: string): bool =
       return false
     else: echo "Please specify y/n (yes/no)"
 
-
-# Solves SUVAT equations based on values and known variables
-proc solve(equationVariables: seq[char], values: seq[int]): int =
+# Finds base SUVAT equations based on values and known variables
+proc findEquation(equationVariables: seq[char], values: seq[float]): float =
   var
     solvers = [['S', 'U', 'V', 'T'], ['V', 'U', 'A', 'T'], ['S', 'U', 'T', 'A'], ['V', 'U', 'A', 'S']]
   echo "Searching for proper equation"
@@ -25,13 +24,70 @@ proc solve(equationVariables: seq[char], values: seq[int]): int =
   # Find base equation to solve
   for i in solvers.low .. solvers.high:
     for x in solvers[i].low .. solvers[i].high:
-
       if solvers[i].contains(equationVariables[x]):
-
         if x == 3:
           echo "Equation found. Solving"
-          echo solvers[i]
 
+          # I am not proud of this code. There are so many switch cases here. Why. I feel like there is a better solution.
+          case i
+          of 0:
+            echo "reordering equation 1"
+            case equationVariables[0]
+            of 'S':
+              echo "Equation: S = ((u + v)/2)*t"
+              return ((values[0] + values[1])/2)*values[2]
+            of 'U':
+              echo "Equation: U = (s/t)*2 + v"
+              return (values[0]/values[2])*2 + values[1]
+            of 'V':
+              echo "Equation: V = (s/t)*2 + u"
+              return (values[0]/values[2])*2 + values[1]
+            of 'T':
+              echo "Equation: T = s/((u + v)/2)"
+              return (values[0]/((values[1] + values[2])/2))
+            else:
+              echo "Invalid Final!"
+              quit(1)
+          of 1:
+            echo "reordering equation 2"
+            case equationVariables[0]
+            of 'V':
+              echo "Equation: V = u + at"
+              return (values[0] + (values[1] * values[2]))
+            of 'U':
+              echo "Equation: U = v + at"
+              return (values[0] + (values[1] * values[2]))
+            of 'A':
+              echo "Equation: A = (v - u)/t"
+              return (values[1] - values[0])/values[2]
+            of 'T':
+              echo "Equation: T = (v - u)/a"
+              return (values[1] - values[0])/values[2]
+            else:
+              echo "Invalid Final!"
+              quit(1)
+          of 2:
+            echo "reordering equation 3"
+            case equationVariables[0]
+            of 'S':
+              echo "Equation: S = u*t + (a/2)*(t^2)"
+            else:
+              echo "Invalid Final!"
+              quit(1)
+          of 3:
+            echo "reordering equation 4"
+            case equationVariables[0]
+            of 'V':
+              echo "Equation: V^2 = u^2 + 2*a*s"
+              return sqrt(pow(values[1], 2) + (2*values[2])*values[0])
+            of 'U':
+              echo "Equation: -U^2 = 2*a*s - v^2"
+              return sqrt(-1*((2*values[2])*values[0]-pow(values[1], 2)))
+            of 'S':
+              echo "Equation: "
+            else:
+              echo "Invalid Final!"
+              quit(1)
       else:
         break
 
@@ -45,7 +101,7 @@ when isMainModule:
     letter: char
     x: int = 0
     final: char
-    suvat: seq[int]
+    suvat: seq[float]
 
   # Find known values
   for i in knownValues.low .. knownValues.high:
@@ -67,15 +123,14 @@ when isMainModule:
     echo "Valid size found. Please input your Values\n"
     for i in 0..len(letters) - 1:
         stdout.write("Value for " & $letters[i] & " -> ")
-        suvat.add(parseInt(readLine(stdin)))
+        suvat.add(parseFloat(readLine(stdin)))
 
     # Find value to evaluate to
     while true:
         stdout.write("What value are your solving for -> ")
         final = readLine(stdin)[0]
 
-        if letters.contains(final) == false:
-
+        if not letters.contains(final):
           case final
           of 's', 'u', 'v', 'a', 't':
             echo "Type an Uppercase value please"
@@ -92,4 +147,4 @@ when isMainModule:
 
   # Solve
   letters.insert(final, 0)
-  echo $final & " has evaluated to " & $solve(letters, suvat)
+  echo $final & " has evaluated to " & $findEquation(letters, suvat)
